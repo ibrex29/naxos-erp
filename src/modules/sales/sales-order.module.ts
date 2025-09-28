7f4aebc3e4ad1a51ebb9a5586bceaa852117026c
ibrahim muhammad <ibrex29@gmail.com>
2025-09-28 08:30:53 +0100

    more

--- src/modules/sales/sales-order.module.ts
+++ src/modules/sales/sales-order.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { SalesOrderService } from "./sales-order.service";
 import { SalesOrderController } from "./sales-order.controller";
 import { PaymentController } from "./payment.controller";
+import { PaymentService } from "./payment.service";
 
 @Module({
   controllers: [SalesOrderController, PaymentController],
-  providers: [SalesOrderService, ],
+  providers: [SalesOrderService,PaymentService ],
 })
 export class SalesOrderModule {}
